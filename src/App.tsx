77a0cdf5ed939ba15a58fd98cacb681ae232f03b
rafaelmjardim <rafaelmjardim@hotmail.com>
2023-06-27 14:04:29 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import './App.scss'
-import { ClipboardList, X, ClipboardEdit } from "lucide-react";
+import { ClipboardList, X } from "lucide-react";
 
 function App() {
+
+  const todoList = [
+    {
+      id: 1,
+      name: 'Primeiro item'
+    },
+    {
+      id: 2,
+      name: 'Segundo item'
+    },
+  ];  
 
   return (
     <>
@@ -9,19 +20,25 @@ function App() {
         <div className='app_container bg-sky-500'>
           <form className='app_container_form' placeholder='Insira sua tarefa'>
             <input type="text" />
-            <button>Enviar</button>
+            <button className='button'>Enviar</button>
           </form>
 
           <ul className='app_container_list'>
-            <li className='app_container_list_item'>
-              <div className="app_container_list_item_left">
-              <ClipboardList size={20}/>
-                <span>Título da tarefa</span>
-              </div>
-              <div className="app_container_list_item_right">
-              <X size={16}/>
-              </div>
-            </li>
+            {
+              todoList.map(todo => (
+                <li className='app_container_list_item' key={todo.id}>
+                  <div className="app_container_list_item_left">
+                    <ClipboardList size={20}/>
+                    <span>{todo.name}</span>
+                  </div>
+                  <div className="app_container_list_item_right">
+                    <div className='app_container_list_item_right_icon' onClick={() => alert('Excluiu')}>
+                      <X size={16}/>
+                    </div>
+                  </div>
+                </li>
+              ))
+            }
           </ul>
         </div>
       </div>
